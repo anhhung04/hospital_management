ca8cdf8f3154d6092ccba237f3461b18088359f2
anhhung04 <dev.hah4@gmail.com>
2024-03-06 18:01:49 +0700

    build: dockerize frontend and backend

--- backend/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ $DEBUG = "1" ]
-then
-    echo "Running in debug mode"
-    uvicorn app:app --reload --host 0.0.0.0 --port 8000
-else
-# Start the server
-uvicorn app:app --host 0.0.0.0 --port 8000 --workers 4
-fi
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import { useEffect, useState } from "react";
 
+function App() {
+  const [text, setText] = useState('');
+  useEffect(
+    () => {
+      fetch('/api/demo/hello')
+        .then((res) => res.json())
+        .then((data) => setText(data.message))
+        .catch((err) => setText(err.message));
+    }, []
+  );
   return (
-    <>
-      <div>Hello World!</div>
-    </>
+    <div>
+      <p>{text}</p>
+    </div>
   );
 }
 
