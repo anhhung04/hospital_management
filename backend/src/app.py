117e540f7b0d31993d649ca00dc31c659b2745a0
anhhung04 <dev.hah4@gmail.com>
2024-03-12 15:30:08 +0700

    feat(backend): add cors and config

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from config import config
 
 from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url='/api/docs' if not config['PROD'] else None,
               redoc_url='/api/redoc' if not config['PROD'] else None,
@@ -13,12 +14,19 @@ POSTGRES_SQL_URL = config['POSTGRES_SQL_URL']
 
 app.add_middleware(DBSessionMiddleware, db_url=POSTGRES_SQL_URL)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config['ALLOWED_HOSTS'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 for file in os.listdir("routes"):
     if not file.startswith('_'):
         module_name, _ = os.path.splitext(file)
         module = __import__(f"routes.{module_name}", fromlist=[module_name])
         app.include_router(module.router, prefix=f"/api/{module_name}")
-
 
 if __name__ == "__main__":
     configs = {
--- backend/src/config.py
+++ backend/src/config.py
@@ -4,5 +4,7 @@ config = {
     "PORT": 8000,
     "HOST": "0.0.0.0",
     "workers": 4,
-    "PROD": os.getenv("PROD", False)
+    "PROD": os.getenv("PROD", False),
+    "ALLOWED_HOSTS": ["*"],
+    "POSTGRES_SQL_URL": f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 }
