76293f33d198d12c889c792d5c76437a771bc530
anhhung04 <dev.hah4@gmail.com>
2024-03-19 19:27:46 +0700

    refactor: prepare test template

--- backend/app/test/login_test.py
+++ backend/app/test/login_test.py
@@ -1,66 +1,8 @@
 from unittest import TestCase
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
-from repository.schemas import Base
-from repository import get_db, get_redis
-from app import app
+from test import override_get_db, gen_password, gen_username, client
 from util.crypto import hash_password
 from uuid import uuid4
-from test import gen_password, gen_username
 from repository.schemas.user import User
-
-
-SQLALCHEMY_DATABASE_URL = "sqlite://"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-class RedisLocal:
-    cache: dict
-
-    def __init__(self):
-        self.cache = {}
-
-    def set(self, key, value, ex):
-        self.cache[key] = value
-
-    def delete(self, key):
-        if key in self.cache:
-            del self.cache[key]
-
-    def get(self, key):
-        return self.cache.get(key)
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-def override_get_redis():
-    try:
-        yield RedisLocal()
-    finally:
-        pass
-
-
-app.dependency_overrides[get_redis] = override_get_redis
-app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
 
 class TestDemo(TestCase):
     def test_login(self):
@@ -74,3 +16,10 @@ class TestDemo(TestCase):
                           json={"username": u, "password": p})
         self.assertEqual(res.status_code, 200)
         self.assertTrue("access_token" in res.json()['data'])
+
+    def test_login_fail(self):
+        res = client.post("/api/auth/login",
+                          json={"username": gen_username(), "password": gen_password()})
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(res.json()['message'],
+                         "Incorrect username or password")
