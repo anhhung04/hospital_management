aad4952527d4400addb9171f0d20a4b6d5ac1c94
anhhung04 <dev.hah4@gmail.com>
2024-03-20 16:04:43 +0700

    feat: define permission for future use

---
+++ backend/app/permissions/const.py
@@ -0,0 +1,9 @@
+# constants
+
+Allow = "ALLOW"  # acl "allow" action
+Deny = "DENY"  # acl "deny" action
+
+Everyone = "system:everyone"  # user principal for everyone
+Authenticated = "system:authenticated"  # authenticated user principal
+
+
