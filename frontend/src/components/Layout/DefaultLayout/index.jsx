2099647f5696535f1aa3cd5c4ed4af011661e211
anhhung04 <dev.hah4@gmail.com>
2024-03-26 14:22:48 +0700

    fix: conflict in frontend

--- frontend/src/components/Layout/DefaultLayout/index.jsx
+++ frontend/src/components/Layout/DefaultLayout/index.jsx
@@ -2,15 +2,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function DefaultLayout({children}) {
-    const [current_content, setCurrent_content] = useState("general");  
-    function handleClick(content) {
-        setCurrent_content(content);
-    }
+    // const [current_content, setCurrent_content] = useState("general");  
+    // function handleClick(content) {
+    //     setCurrent_content(content);
+    // }
     return (<div className="flex flex-col h-full w-full justify-between mb-0">
         <Header/>
-        <div class="flex flex-row">
-            <div class="flex-1 p-10">
+        <div className="flex flex-row">
+            <div className="flex-1 p-10">
                 {children}
+            </div>
         </div>
         <Footer/>
     </div> );
