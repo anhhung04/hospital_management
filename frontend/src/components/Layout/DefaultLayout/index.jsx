ba5fbce07f77793f627ac09426f0e6741a9273f9
anhhung04 <dev.hah4@gmail.com>
2024-03-26 14:27:28 +0700

    feat: add sidebar

--- frontend/src/components/Layout/DefaultLayout/index.jsx
+++ frontend/src/components/Layout/DefaultLayout/index.jsx
@@ -1,14 +1,17 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Sidebar from "./SideBar";
+import { useState } from "react";
 
 function DefaultLayout({children}) {
-    // const [current_content, setCurrent_content] = useState("general");  
-    // function handleClick(content) {
-    //     setCurrent_content(content);
-    // }
+    const [current_content, setCurrent_content] = useState("general");  
+    function handleClick(content) {
+        setCurrent_content(content);
+    }
     return (<div className="flex flex-col h-full w-full justify-between mb-0">
         <Header/>
         <div className="flex flex-row">
+            <Sidebar current_content={current_content} handleClick={handleClick}/>
             <div className="flex-1 p-10">
                 {children}
             </div>
