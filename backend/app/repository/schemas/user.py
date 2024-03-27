68506e717af400e3a0523745ac57908b119169b9
anhhung04 <dev.hah4@gmail.com>
2024-03-27 13:03:58 +0700

    fix: change user init column

--- backend/app/repository/schemas/user.py
+++ backend/app/repository/schemas/user.py
@@ -1,15 +1,11 @@
-<<<<<<< Updated upstream
-from sqlalchemy import Column, String
-=======
 from sqlalchemy import String, Integer, Date
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
->>>>>>> Stashed changes
 from repository.schemas import Base
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(String, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    id = mapped_column(String, primary_key=True, index=True)
+    username = mapped_column(String, unique=True, index=True)
+    password = mapped_column(String)
