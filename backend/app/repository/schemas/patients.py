532bf24b38d5eb49843cf2743692202ca87e54b9
tuananh1342004 <nnta1342004@gmail.com>
2024-03-30 16:27:01 +0700

    patient+user schemas

---
+++ backend/app/repository/schemas/patients.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from repository.schemas import Base
+
+class Patient(Base):
+    __tablename__ = 'patients'
+
+    medID = mapped_column(Integer, primary_key=True, index=True, unique=True)
+    user_id = mapped_column(String, ForeignKey('users.id'), index=True)
+    medical_record_id = mapped_column(Integer, primary_key=True, index=True, unique=True)
+    ssn = mapped_column(Integer, ForeignKey('users.ssn'), unique=True)
+    weight = mapped_column(Integer)
+    height = mapped_column(Integer)
+    note = mapped_column(String)
+    user = relationship("User", back_populates="patients", uselist=False)
--- backend/app/repository/schemas/user.py
+++ backend/app/repository/schemas/user.py
@@ -1,9 +1,43 @@
-from sqlalchemy import Column, String
+from sqlalchemy import String, Integer, Date
+from sqlalchemy import Enum as SQLAlchemyEnum
+from sqlalchemy.orm import mapped_column
 from repository.schemas import Base
+from enum import Enum
+
+
+class UserType(Enum):
+    EMPLOYEE = 0
+    PATIENT = 1
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(String, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    id = mapped_column(String, primary_key=True, index=True)
+    ssn = mapped_column(Integer, unique=True, index=True)
+    username = mapped_column(String, unique=True, index=True)
+    password = mapped_column(String)
+    phone_number = mapped_column(Integer)
+    user_type = mapped_column(SQLAlchemyEnum(UserType))
+    gender = mapped_column(String)
+    last_name = mapped_column(String)
+    first_name = mapped_column(String)
+    birth_date = mapped_column(Date)
+    nation = mapped_column(String)
+    province = mapped_column(String)
+    district = mapped_column(String)
+    ward = mapped_column(String)
+    address = mapped_column(String)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
