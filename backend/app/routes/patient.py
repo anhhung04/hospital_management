ae729622c007786d019876de75c567e73cbdf474
anhhung04 <dev.hah4@gmail.com>
2024-04-02 16:16:07 +0700

    fix: patient

--- backend/app/routes/patient.py
+++ backend/app/routes/patient.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, HTTPException
 from services.patient import PatientService
 from repository import Storage
 from fastapi import Query
@@ -9,9 +9,14 @@ router = APIRouter()
 
 @router.get("/patient")
 async def list_patients(
+    request: Request,
     page=Annotated[int, Query(gt=0)],
     patient_per_page=Annotated[int, Query(gt=0)],
     db_sess=Depends(Storage.get)
 ):
+    if not request.state.user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    if request.state.user.role == 'EMPLOYEE':
+        raise HTTPException(status_code=403, detail="Forbidden")    
     patients = PatientService(db_sess).get_patients(page, patient_per_page)
     return patients
--- backend/app/services/patient.py
+++ backend/app/services/patient.py
@@ -6,10 +6,20 @@ class PatientService:
     def __init__(self, session: Session):
         self._repo = PatientRepo(session)
 
-    def get_patients(self, page: int = 1, patient_per_page: int = 10):
+    def get_patients(self, page: int = 1, patient_per_page: int = 10) -> PatientResponseMode:
         if page < 1:
             page = 1
         if patient_per_page < 1:
             patient_per_page = 10
         patients = self._repo.list_patient(page=page, patient_per_page=patient_per_page)
-        
+        patients = list(map(
+            lambda p: PatientResponseMode(
+                id=p.user_id,
+                full_name=f"{p.personal_info.first_name} {
+                    p.personal_info.last_name}",
+                phone_number=p.personal_info.phone_number,
+                medical_record=p.medical_record.id
+            ),
+            patients
+        ))
+        return patients
