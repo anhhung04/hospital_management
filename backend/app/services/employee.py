3d1d5f7d21d6eefbfab954952cb8302a0800f4e5
qbao <dangquocbao9504@gmail.com>
2024-04-09 08:48:03 +0700

    delete print

--- backend/app/services/employee.py
+++ backend/app/services/employee.py
@@ -133,7 +133,6 @@ class EmployeeService:
             employee_update
         )
         if error:
-            print(error)
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Error in update employee information"
