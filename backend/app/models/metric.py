e53571f60ced47a592f2652593ba40c064eff110
anhhung04 <dev.hah4@gmail.com>
2024-05-18 14:04:57 +0700

    feat: count employee

--- backend/app/models/metric.py
+++ backend/app/models/metric.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 class MetricModel(BaseModel):
     num_patients: int
     num_doctors: int
+    num_nurses: int
+    num_managers: int
+    num_other: int
     num_employee: int
     patients_per_day: list[int]
 
