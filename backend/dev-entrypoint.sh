37c34002a86671453f4549b302535ad8a05b8c44
Tran Dang Bao <trandangbao2004@gmail.com>
2024-05-18 18:06:29 +0700

    BE/DELETE BE

--- backend/dev-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo 'Starting migration...'
-alembic upgrade head
-
-while true; do
-    python3 -m debugpy --listen 0.0.0.0:8888 app.py
-    echo 'App crashed, restarting in 5 seconds...'
-    sleep 5
-done
