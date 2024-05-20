bbd01c4e0fd9da45a2c75c01789c7a55b830b787
qbao <dangquocbao9504@gmail.com>
2024-05-20 16:30:36 +0700

    demo: create pull request

--- backend/app/routes/demo.py
+++ backend/app/routes/demo.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/hello")
 async def index():
     return {"message": "Hello, World!"}
+
+@router.get("/demo")
+async def demo():
+    return {"demo": "This is a demo route!"}
